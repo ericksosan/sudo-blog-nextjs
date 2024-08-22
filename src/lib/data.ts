5b44f34440b3768022a7e7ff5f483a31a26c6e7a
ericksosan <erickdev03@gmail.com>
2024-08-21 22:17:09 -0400

    fix: fixed fetch data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,69 +1,39 @@
-import type { Post, Comment, Category } from '@/types';
-import axios from 'axios';
-import { z } from 'zod';
-import { writePostSchema } from './zod';
+import type { Post, Comment, Category } from '@/types'
+import axios from 'axios'
+import { z } from 'zod'
+import { writePostSchema } from './zod'
 
 export const api = axios.create({
   baseURL: process.env.BACK_BASE_URL,
-});
+})
 
 export const fetchPosts = async (): Promise<Post[]> => {
-  try {
-    const response = await api.get<Post[]>('/post')
-    return response.data
-  } catch (error) {
-    console.error('Error fetching posts:', error)
-    return []
-  }
+  const response = await api.get<Post[]>('/post')
+  return response.data
 }
 
 export const fetchPost = async (slug: string): Promise<Post> => {
-  try {
-    const response = await api.get<Post>(`/post/${slug}`)
-    return response.data
-  } catch (error) {
-    console.error('Error fetching post:', error)
-    throw new Error('Failed to fetch post')
-  }
+  const response = await api.get<Post>(`/post/${slug}`)
+  return response.data
 }
 
 export const fetchCommantsOfPost = async (postId: string): Promise<Comment[]> => {
-  try {
-    const response = await api.get<Comment[]>(`/post/comment/${postId}`)
-    return response.data
-  } catch (error) {
-    console.error('Error fetching comments post:', error)
-    throw new Error('Failed to fetch comments post')
-  }
+  const response = await api.get<Comment[]>(`/post/comment/${postId}`)
+  return response.data
 }
 
 export const fetchMostViewedPosts = async (): Promise<Post[]> => {
-  try {
-    const response = await api.get<Post[]>('/post/most-viewed')
-    return response.data
-  } catch (error) {
-    console.error('Error fetching posts:', error)
-    return []
-  }
+  const response = await api.get<Post[]>('/post/most-viewed')
+  return response.data
 }
 type WritePostData = z.infer<typeof writePostSchema>
 
 export const fetchCreatePost = async (data: WritePostData): Promise<Post> => {
-  try {
-    const response = await api.post<Post>('/api/post/write', data);
-    return response.data;
-  } catch (error) {
-    console.error('Error creating post:', error);
-    throw new Error('Failed to create post');
-  }
-};
+  const response = await api.post<Post>('/api/post/write', data)
+  return response.data
+}
 
 export const fetchCategories = async (): Promise<Category[]> => {
-  try {
-    const response = await api.get<Category[]>('/api/post/category');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching categories:', error);
-    throw new Error('Failed to fetch categories');
-  }
-};
+  const response = await api.get<Category[]>('/api/post/category')
+  return response.data
+}
